c6a9844924bc07ac40297928a566efc0dd4b27e0
ZY <zy30651@qq.com>
2022-07-19 17:55:09 +0800

    登陆模块,表单校验,qq登陆等

--- src/components/app-topnav.vue
+++ src/components/app-topnav.vue
@@ -1,82 +1,98 @@
 <template>
-    <nav class="app-topnav">
-        <div class="container">
-            <ul>
-                <template v-if="profile.token">
-                    <li><a href="javascript:;"><i class="iconfont icon-user"></i>
-                            {{ profile.nickname }}
-                        </a>
-                    </li>
-                    <li><a href="javascript:;">退出登录</a></li>
-                </template>
-                <template v-else>
-                    <li><a href="javascript:;">请先登录</a></li>
-                    <li><a href="javascript:;">免费注册</a></li>
-                </template>
-                <li><a href="javascript:;">我的订单</a></li>
-                <li><a href="javascript:;">会员中心</a></li>
-                <li><a href="javascript:;">帮助中心</a></li>
-                <li><a href="javascript:;">关于我们</a></li>
-                <li><a href="javascript:;"><i class="iconfont icon-phone"></i>手机版</a></li>
-            </ul>
-        </div>
-    </nav>
+  <nav class="app-topnav">
+    <div class="container">
+      <ul>
+        <template v-if="profile.token">
+          <li>
+            <a href="javascript:;">
+              <i class="iconfont icon-user"></i>
+              {{ profile.nickname }}
+            </a>
+          </li>
+          <li><a href="javascript:;" @click="logout()">退出登录</a></li>
+        </template>
+        <template v-else>
+          <li>
+            <router-link to="/login">请先登录</router-link>
+          </li>
+          <li>
+            <router-link to="/register">免费注册</router-link>
+          </li>
+        </template>
+        <li><a href="javascript:;">我的订单</a></li>
+        <li><a href="javascript:;">会员中心</a></li>
+        <li><a href="javascript:;">帮助中心</a></li>
+        <li><a href="javascript:;">关于我们</a></li>
+        <li>
+          <a href="javascript:;">
+            <i class="iconfont icon-phone"></i>
+            手机版
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 import { computed } from 'vue'
 export default {
-    name: 'AppTopNav',
-    setup() {
-        const store = useStore()
+  name: 'AppTopNav',
+  setup() {
+    const store = useStore()
 
-        // 使用vuex中的state需要设置计算属性;否则就$store.state.user.profile
-        const profile = computed(() => {
-            return store.state.user.profile
-        })
-        return { profile }
+    const router = useRouter()
+    const logout = () => {
+      store.commit('user/setUser', {})
+      router.push('/login')
     }
+
+    // 使用vuex中的state需要设置计算属性;否则就$store.state.user.profile
+    const profile = computed(() => {
+      return store.state.user.profile
+    })
+    return { profile, logout }
+  },
 }
-</script>  
-
-<script setup>
-
 </script>
+
+<script setup></script>
 
 <style lang="less" scoped>
 .app-topnav {
-    background: #333;
+  background: #333;
 
-    ul {
-        display: flex;
-        height: 53px;
-        justify-content: flex-end;
-        align-items: center;
+  ul {
+    display: flex;
+    height: 53px;
+    justify-content: flex-end;
+    align-items: center;
 
-        li {
-            a {
-                padding: 0 10px;
-                line-height: 1;
-                color: white;
-                display: inline-block;
+    li {
+      a {
+        padding: 0 10px;
+        line-height: 1;
+        color: white;
+        display: inline-block;
 
-                i {
-                    font-size: 14px;
-                    margin-right: 2px;
-                }
-
-                &:hover {
-                    color: @xtxColor
-                }
-            }
-
-            ~li {
-                a {
-                    border-left: 2px solid #666
-                }
-            }
+        i {
+          font-size: 14px;
+          margin-right: 2px;
         }
+
+        &:hover {
+          color: @xtxColor;
+        }
+      }
+
+      ~ li {
+        a {
+          border-left: 2px solid #666;
+        }
+      }
     }
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import SubCategory from '@/views/category/sub.vue'
 const Layout = () => import('@/views/Layout.vue')
 const Home = () => import('@/views/home/index.vue')
 const Goods = () => import('@/views/goods/index.vue')
+const Login = () => import('@/views/Login/index.vue')
 
 const routes = [
   // 一级路由布局容器
@@ -19,6 +20,8 @@ const routes = [
       { path: '/product/:id', component: Goods },
     ],
   },
+
+  { path: '/login', component: Login },
 ]
 // vue2.0 new VueRouter({}) 创建路由实例
 // vue3.0 createRouter({})
